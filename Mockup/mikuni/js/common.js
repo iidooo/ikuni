4fe10c4084436c861a7b1e4fc592fc6cbed89e49
wangyixian <ouitiken@gmail.com>
2016-04-30 15:35:36 +0800

    搭建假画面的结构

---
+++ Mockup/mikuni/js/common.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Ethan on 16/4/6.
+ */
+var clientURL = "http://localhost:63342/mikuni";
+//var server = "http://www.iidooo.com";
+var activeNavItem = "index";
---
+++ Mockup/mikuni/js/widgets/header.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Ethan on 16/4/6.
+ */
+$(function () {
+    $('header').load(clientURL + '/widgets/header.html');
+
+
+});
