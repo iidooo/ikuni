aa09448880818f69f6ae69ae0d0f95656765c9cf
wangyixian <ouitiken@gmail.com>
2016-05-11 21:43:18 +0800

    按照review结果修正

--- Mockup/mikuni/js/apply-asset.js
+++ Mockup/mikuni/js/apply-asset.js
@@ -14,7 +14,9 @@ $(function () {
     });
 
     $("#btnSetApprovalUser").bind("click", function(){
-        var $select = $("<select class='form-control'><option>审批者2:社长</option></select>");
-        $("#btnSetApprovalUser").before($select);
+        var $select1 = $("<select class='form-control'><option>开发部</option><option>销售部</option><option>研发部</option></select>");
+        var $select2 = $("<select class='form-control'><option>审批者2:社长</option></select>");
+        $("#btnSetApprovalUser").before($select1);
+        $("#btnSetApprovalUser").before($select2);
     });
 });
--- Mockup/mikuni/js/apply-purchase.js
+++ Mockup/mikuni/js/apply-purchase.js
@@ -1,21 +1,15 @@
 /**
- * Created by Ethan on 16/4/30.
+ * Created by Ethan on 16/5/11.
  */
 $(function () {
-    $('.form_datetime').datetimepicker({
-        weekStart: 1,
-        todayBtn: 1,
-        autoclose: 1,
-        todayHighlight: 1,
-        startView: 2,
-        forceParse: 0,
-        format: 'yyyy-mm-dd hh:ii:ss'
-    });
-
     $("#btnSetApprovalUser").bind("click", function(){
         var $select1 = $("<select class='form-control'><option>开发部</option><option>销售部</option><option>研发部</option></select>");
         var $select2 = $("<select class='form-control'><option>审批者2:社长</option></select>");
         $("#btnSetApprovalUser").before($select1);
         $("#btnSetApprovalUser").before($select2);
+    });
+
+    $("#btnAppend").bind("click", function(){
+        $("#tblApply tr:last").clone(true).appendTo('#tblApply');
     });
 });
